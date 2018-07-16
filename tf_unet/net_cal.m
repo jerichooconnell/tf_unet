3a6bed5e2659fd639e0c62af322f4650d9a495fd
jerichooconnell <jericho.oconnell@gmail.com>
2018-07-16 14:51:52 -0700

    giving up

--- tf_unet/net_cal.m
+++ tf_unet/net_cal.m
@@ -7,7 +7,7 @@ x_train = reshape(x,numel(x)/4,4);
 
 %save('x_train','x_train')
 
-net = cascadeforwardnet(55); %55
+net = cascadeforwardnet(150); %55
 net.trainParam.showWindow = 0;
 [net, perf] = train(net, x_train', t); %JO
 
